7ae8317055bbe59e0d389a22013347d3d5d086c7
Max Moiseev <max@maxmoiseev.com>
2025-01-18 12:51:43 -0800

    Even more updates too different to mention

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set -gx EDITOR vim
 end
+
 set -gx HOMEBREW_GITHUB_API_TOKEN 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
-set -gx EDITOR vim
+
+# Using a global variable so that $HOME is expanded
+set -g --prepend fish_user_paths $HOME/bin $HOME/.cargo/bin $HOME/.ghcup/bin
