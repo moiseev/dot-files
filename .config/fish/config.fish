e5192bb456ca581bf03ff0573f4bcc97e4232ef1
Max Moiseev <max@maxmoiseev.com>
2025-01-27 14:42:39 -0800

    Remove unused token

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,5 @@ if status is-interactive
     set -gx EDITOR vim
 end
 
-set -gx HOMEBREW_GITHUB_API_TOKEN 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
-
 # Using a global variable so that $HOME is expanded
 set -g --prepend fish_user_paths $HOME/bin $HOME/.cargo/bin $HOME/.ghcup/bin
