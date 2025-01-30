a9968e12ba436dc81105fb2389f29cab7768a9bb
Max Moiseev <max@maxmoiseev.com>
2025-01-30 12:19:45 -0800

    Maximize and untabify the wezterm window

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,4 +10,12 @@ config.font = wezterm.font 'JetBrains Mono'
 config.font_size = 14.0
 
 config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
+
+wezterm.on('gui-startup', function (cmd)
+  local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+config.hide_tab_bar_if_only_one_tab = true
+
 return config
