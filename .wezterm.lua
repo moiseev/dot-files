61b7d90ad9c4afd023881447ed6bd5f7ebab7cba
Max Moiseev <max@maxmoiseev.com>
2025-02-11 14:19:31 -0800

    Tmux layout setting + better opacity in wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Catppuccin Mocha'
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.95
 config.macos_window_background_blur = 20
 
 config.font = wezterm.font 'JetBrains Mono'
